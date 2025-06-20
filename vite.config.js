62cb83e4ae89d29b81c9cc8bd277132c5fd7e012
saisrinivast <srinivassai617@gmail.com>
2025-06-20 15:18:36 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
+// vite.config.js
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',
-    port: 5173,
     proxy: {
       '/api': {
         target: 'http://localhost:10000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '/api'),
+        changeOrigin: true
       }
     }
   }
